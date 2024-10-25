b09080294f4826c750fd508ce7a8af4785666b32
Víctor Ponz <victorponz@gmail.com>
2024-10-25 08:10:28 +0200

    Versión final

---
+++ src/test/SampleTest.java
@@ -0,0 +1,9 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class SampleTest {
+
+    @org.junit.jupiter.api.Test
+    void letraDNI() {
+        assertEquals(17, Sample.suma(5, 12));
+    }
+}
